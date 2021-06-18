4a0847be9afcc50bed173f138d6c4661210414b6
Alessandro Di Crescenzo <adicrescenzo79@gmail.com>
2021-06-18 10:39:57 +0200

    prova ale 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -88,7 +88,7 @@
                     <p>Nicolò</p>
                     <p>alessandro</p>
                     <p>andrea</p>
-                    <a href="#">ciao vsevolod</a>            
+                    <a href="#">ciao vsevolod</a>
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
@@ -97,6 +97,7 @@
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://vapor.laravel.com">Vapor</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <p>prova ale 2</p>
                 </div>
             </div>
         </div>
